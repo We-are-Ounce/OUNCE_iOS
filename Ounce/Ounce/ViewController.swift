4500d3304cb88f695cacf9c57f4f8cbfece1ded1
juyeonblue <jyp0211@ajou.ac.kr>
2020-06-30 15:19:18 +0900

    first commit

---
+++ Ounce/Ounce/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ounce
+//
+//  Created by 박주연 on 2020/06/30.
+//  Copyright © 2020 박주연. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

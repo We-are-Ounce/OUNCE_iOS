f540d9ed7e4c61804008283e7333b122541229fd
Junhyeon <oh.junhyeon95@gmail.com>
2020-07-01 16:13:13 +0900

    UPDATE: 폴더링 완료

---
+++ Ounce/Ounce/Example+Reference/Cells/ReferenceCVCell.swift
@@ -0,0 +1,18 @@
+//
+//  ReferenceCVCell.swift
+//  Ounce
+//
+//  Created by Junhyeon on 2020/07/01.
+//  Copyright © 2020 박주연. All rights reserved.
+//
+
+import UIKit
+
+class ReferenceCVCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}

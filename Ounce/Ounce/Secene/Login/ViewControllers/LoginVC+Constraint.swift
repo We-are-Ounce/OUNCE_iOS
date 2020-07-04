d1967649da53c3a33a128c528e15b91f7fe6b15b
Junhyeon <oh.junhyeon95@gmail.com>
2020-07-05 01:29:26 +0900

    UPDATE: #1 뷰 구현 완료
    추후 로고 추가 예정

--- Ounce/Ounce/Secene/Login/ViewControllers/LoginVC+Constraint.swift
+++ Ounce/Ounce/Secene/Login/ViewControllers/LoginVC+Constraint.swift
@@ -1,9 +1,113 @@
-//
-//  LoginVC+Constraint.swift
-//  Ounce
-//
-//  Created by Junhyeon on 2020/07/04.
-//  Copyright © 2020 박주연. All rights reserved.
-//
+import UIKit
 
-import Foundation
+import SnapKit
+
+extension LoginVC {
+    func constraint() {
+        self.view.addSubview(logoImageView)
+        self.view.addSubview(idGuideLabel)
+        self.view.addSubview(idErrorGuideLabel)
+        self.view.addSubview(idTextField)
+        self.view.addSubview(idTextFieldGuideView)
+        self.view.addSubview(pwGuideLabel)
+        self.view.addSubview(pwErrorGuideLabel)
+        self.view.addSubview(pwTextField)
+        self.view.addSubview(pwTextFieldGuideView)
+        self.view.addSubview(findGuideButton)
+        self.view.addSubview(loginButton)
+        self.view.addSubview(signUpGuideLabel)
+        self.view.addSubview(signUpButton)
+        
+        logoImageView.snp.makeConstraints { (make) in
+            make.leading.equalToSuperview()
+            make.top.equalToSuperview().offset(88)
+            make.width.equalToSuperview()
+            make.height.equalTo(27.5)
+        }
+        
+        idGuideLabel.snp.makeConstraints { (make) in
+            make.leading.equalToSuperview().offset(16)
+            make.top.equalTo(logoImageView.snp.bottom).offset(43.6)
+            make.width.equalToSuperview()
+            make.height.equalTo(24)
+        }
+        
+        idErrorGuideLabel.snp.makeConstraints { (make) in
+            make.leading.equalToSuperview()
+            make.top.equalToSuperview()
+            make.width.equalToSuperview()
+            make.height.equalTo(27.5)
+        }
+        
+        idTextField.snp.makeConstraints { (make) in
+            make.leading.equalToSuperview().offset(16)
+            make.top.equalTo(idGuideLabel.snp.bottom).offset(7)
+            make.width.equalTo(self.view.frame.width / 3 * 2)
+            make.height.equalTo(24)
+        }
+        
+        idTextFieldGuideView.snp.makeConstraints { (make) in
+            make.leading.equalToSuperview().offset(16)
+            make.trailing.equalToSuperview().offset(-16)
+            make.top.equalTo(idTextField.snp.bottom).offset(8)
+            make.height.equalTo(1)
+        }
+
+        pwGuideLabel.snp.makeConstraints { (make) in
+            make.leading.equalToSuperview().offset(16)
+            make.top.equalTo(idGuideLabel.snp.bottom).offset(66)
+            make.width.equalToSuperview()
+            make.height.equalTo(24)
+        }
+        
+        pwErrorGuideLabel.snp.makeConstraints { (make) in
+            make.leading.equalToSuperview()
+            make.top.equalToSuperview()
+            make.width.equalToSuperview()
+            make.height.equalTo(27.5)
+        }
+        
+        pwTextField.snp.makeConstraints { (make) in
+            make.leading.equalToSuperview().offset(16)
+            make.top.equalTo(pwGuideLabel.snp.bottom).offset(7)
+            make.width.equalTo(self.view.frame.width / 3 * 2)
+            make.height.equalTo(24)
+        }
+        
+        pwTextFieldGuideView.snp.makeConstraints { (make) in
+            make.leading.equalToSuperview().offset(16)
+            make.trailing.equalToSuperview().offset(-16)
+            make.top.equalTo(pwTextField.snp.bottom).offset(8)
+            make.height.equalTo(1)
+        }
+
+        findGuideButton.snp.makeConstraints { (make) in
+            make.trailing.equalToSuperview().offset(-24)
+            make.top.equalTo(pwTextFieldGuideView.snp.bottom).offset(6)
+            make.width.equalTo(129)
+            make.height.equalTo(19)
+        }
+        
+        loginButton.snp.makeConstraints { (make) in
+            make.leading.equalToSuperview().offset(16)
+            make.trailing.equalToSuperview().offset(-16)
+            make.top.equalTo(pwGuideLabel.snp.bottom).offset(104)
+            make.height.equalTo(48)
+        }
+        
+        signUpGuideLabel.snp.makeConstraints { (make) in
+            make.leading.equalToSuperview().offset(95)
+            make.top.equalTo(loginButton.snp.bottom).offset(6)
+            make.width.equalTo(131)
+            make.height.equalTo(19)
+        }
+        
+        signUpButton.snp.makeConstraints { (make) in
+            make.leading.equalTo(signUpGuideLabel.snp.trailing).offset(6)
+            make.top.equalTo(loginButton.snp.bottom).offset(6)
+            make.width.equalTo(48)
+            make.height.equalTo(19)
+        }
+        
+    }
+}
--- Ounce/Ounce/Secene/Login/ViewControllers/LoginVC.swift
+++ Ounce/Ounce/Secene/Login/ViewControllers/LoginVC.swift
@@ -8,18 +8,119 @@
 
 import UIKit
 
+import Then
+
 class LoginVC: UIViewController {
     
     // MARK: - UI components
     
+    let logoImageView = UIImageView().then {
+        $0.contentMode = .scaleAspectFit
+    }
+    
+    let idGuideLabel = UILabel().then {
+        $0.font = Font.guideLabel
+        print($0.font.pointSize)
+        $0.text = "아이디"
+        $0.textColor = .signatureColor
+    }
+    
+    let idErrorGuideLabel = UILabel().then {
+        $0.font = Font.smallGuideLabel
+        $0.text = "아이디 혹은 비밀번호가 잘못되었습니다."
+        $0.alpha = 0
+        $0.textColor = .red
+    }
+    
+    let idTextField = UITextField().then {
+        $0.font = Font.textField
+        $0.placeholder = "Ounce"
+    }
+    
+    let idTextFieldGuideView = UIView().then {
+        $0.backgroundColor = .brownGreyColor
+    }
+    
+    let pwGuideLabel = UILabel().then {
+        $0.font = Font.guideLabel
+        $0.text = "비밀번호"
+        $0.textColor = .signatureColor
+    }
+    
+    let pwErrorGuideLabel = UILabel().then {
+        $0.font = Font.smallGuideLabel
+        $0.text = "아이디 혹은 비밀번호가 잘못되었습니다."
+        $0.alpha = 0
+        $0.textColor = .red
+    }
+    
+    let pwTextField = UITextField().then {
+        $0.font = Font.textField
+        $0.placeholder = "OuncePW"
+        $0.isSecureTextEntry = true
+    }
+    
+    let pwTextFieldGuideView = UIView().then {
+        $0.backgroundColor = .brownGreyColor
+    }
+    
+    let findGuideButton = UIButton().then {
+        $0.setTitle("비밀번호를 잊으셨나요?", for: .normal)
+        $0.setTitleColor(.brownGreyColor, for: .normal)
+        $0.titleLabel?.font = Font.smallGuideLabel
+        $0.addTarget(self, action: #selector(tapFindButton), for: .touchUpInside)
+    }
+    
+    let loginButton = UIButton().then {
+        $0.backgroundColor = .signatureColor
+        $0.setTitle("로그인", for: .normal)
+        $0.makeRounded(cornerRadius: 8)
+        $0.addTarget(self, action: #selector(tapSignInButton), for: .touchUpInside)
+    }
+    
+    let signUpGuideLabel = UILabel().then {
+        $0.font = Font.smallGuideLabel
+        $0.text = "ounce가 처음이신가요?"
+    }
+
+    let signUpButton = UIButton().then {
+        $0.setTitle("회원가입", for: .normal)
+        $0.setTitleColor(.signatureColor, for: .normal)
+        $0.titleLabel?.font = Font.smallButtonLabel
+        $0.addTarget(self, action: #selector(tapSignUpButton), for: .touchUpInside)
+    }
+    
     // MARK: - Variables and Properties
+        
+    let name = NSMutableAttributedString(string: "회원가입")
     
     // MARK: - Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        constraint()
+
+        name.addAttribute(NSAttributedString.Key.underlineStyle,
+        value: NSUnderlineStyle.single.rawValue,
+        range: NSMakeRange(0, 4))
+        signUpButton.setAttributedTitle(name, for: .normal)
+
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
+          self.view.endEditing(true)
+    }
+    
+    @objc func tapFindButton() {
+        print(#function)
+    }
+    
+    @objc func tapSignInButton() {
+        print(#function)
+    }
+    
+    @objc func tapSignUpButton() {
+        print(#function)
+    }
 }

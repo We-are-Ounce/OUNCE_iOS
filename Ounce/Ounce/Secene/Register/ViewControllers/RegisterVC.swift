e9773b3571e01b1e68c200b5361c1f4454cb2cf8
Junhyeon <oh.junhyeon95@gmail.com>
2020-07-07 13:51:24 +0900

    create : 고양이 등록 뷰 생성

---
+++ Ounce/Ounce/Secene/Register/ViewControllers/RegisterVC.swift
@@ -0,0 +1,30 @@
+//
+//  RegisterVC.swift
+//  Ounce
+//
+//  Created by Junhyeon on 2020/07/07.
+//  Copyright © 2020 박주연. All rights reserved.
+//
+
+import UIKit
+
+class RegisterVC: UIViewController {
+    
+    // MARK: - UI components
+    
+    // MARK: - Variables and Properties
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+}
+
+// MARK: - Helpers 메소드 모두 따로 작성해주세요
+extension RegisterVC {
+    
+}

469b57217c7783ae16e39c4763d6a12926863878
Junhyeon <oh.junhyeon95@gmail.com>
2020-07-13 19:01:36 +0900

    create : #67 서버  연동 베이스 파일 작성

---
+++ Ounce/Ounce/Global/Models/ResponseResult.swift
@@ -0,0 +1,26 @@
+//
+//  ResponseResult.swift
+//  Ounce
+//
+//  Created by Junhyeon on 2020/07/13.
+//  Copyright © 2020 박주연. All rights reserved.
+//
+
+import Foundation
+
+struct ResponseResult<T: Codable>: Codable {
+    var success: Bool
+    var message: String?
+    var data: [T]?
+}
+
+struct ResponseSimpleResult<T: Codable>: Codable {
+    var success: Bool
+    var message: String
+    var data: T?
+}
+
+struct ResponseTempResult: Codable {
+    var success: Bool
+    var message: String
+}
